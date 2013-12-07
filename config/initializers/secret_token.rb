955e5f94bdf6557e534c590fa168a7341776aa0e
Kenny Kline <kline.kenn@gmail.com>
2013-12-06 21:55:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportsalliance::Application.config.secret_key_base = '3202b07293a7efa3522c9961454ecbd563625a0a82520922eb8fa135beff287ff8159beb5f0f644e7332ced4f8fd8ba8f139a03f761adc8a1dd860372ca98d64'
